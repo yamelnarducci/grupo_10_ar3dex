a0ad5cda6f3b80c05672cedbb376e14aa6d2f934
EricMartinez <ericsantiago.m7@gmail.com>
2023-11-15 18:41:57 -0300

    subo header

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const PORT = 3030;
 
 app.use(express.static(path.join(__dirname,'public')));
 
-app.get('/',(req,res) => res.sendFile(path.join(__dirname,'views','index.html')))
+app.get('/',(req,res) => res.sendFile(path.join(__dirname,'views','/partials/header.html')))
 app.get('/register',(req,res) => res.sendFile(path.join(__dirname,'views','register.html')))
 app.get('/login',(req,res) => res.sendFile(path.join(__dirname,'views','login.html')))
 app.get('/product-detail',(req,res) => res.sendFile(path.join(__dirname,'views','productDetail.html')))
