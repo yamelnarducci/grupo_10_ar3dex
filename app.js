4d2598d696474c777747b4d1d8c8cbc1c13f9364
EricMartinez <ericsantiago.m7@gmail.com>
2023-11-16 23:15:35 -0300

    subo productCart

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-const PORT = 3030;
+const PORT = 3031;
 
 app.use(express.static(path.join(__dirname,'public')));
 
