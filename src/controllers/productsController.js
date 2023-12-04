dc8a338d3ca990cdede49749b40592f8a9100ab0
Kevinalexis7 <kevinalex.espinola09@gmail.com>
2023-12-04 20:44:39 -0300

    Añadí products-add y archivo parcial de scripts

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,6 +1,12 @@
 module.exports = {
-    detail: (req,res) => {
+    detail : (req,res) => {
         return res.render('products/product-detail')
+    },
+    add : (req,res) => {
+        return res.render('products/product-add')
+    },
+    edit : (req,res) => {
+        return res.render('products/product-edit')
     }
-    
+
 }
--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-const { detail } = require('../controllers/productsController');
+const { detail, add } = require('../controllers/productsController');
 
 
 /* /productos */
 router
     .get('/detalle/:id', detail)
+    .get('/agregar', add)
 
 module.exports = router;
