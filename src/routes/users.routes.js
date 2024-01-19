0c18dd15f28efc4a707a8b76d877bd5e82538b62
Martín <mcflytix333@gmail.com>
2024-01-19 00:24:31 -0300

    agrego checkAuthUser

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -5,13 +5,14 @@ const userUpload = require('../middlewares/userUpload');
 const userRegisterValidator = require('../../validations/user-register-validator');
 const userLoginValidator = require('../../validations/user-login-validator');
 const checkUserLogin = require('../middlewares/checkUserLogin');
+const checkAuthUser = require('../middlewares/checkAuthUser');
 const router = express.Router();
 
 /* Usuarios */
 router
-    .get('/ingreso', login )
+    .get('/ingreso',checkAuthUser, login )
     .post('/crearUsuario', userUpload.fields([{name : 'userImage'}]), userRegisterValidator, processRegister)
-    .get('/registro', register)
+    .get('/registro', checkAuthUser,register)
     .post('/ingreso',userLoginValidator,processLogin)
     .get('/salir',logout)
     .get('/perfil',checkUserLogin,profile)
